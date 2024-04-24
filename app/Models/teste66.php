2103105cbde31ed630386afe90a602380ffa5a0c
EDU3RDOBR <eduardobfm5@gmail.com>
2024-04-24 10:52:13 -0300

    Update-3.2

---
+++ app/Models/teste66.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class teste66 extends Model
+{
+    protected $guarded = [];
+
+    protected $table = 'teste66';
+
+    protected $guarded = [];
+
+    protected $table = 'teste66';
+
+    use HasFactory;
+}
---
+++ app/teste66.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class teste66 extends Model
+{
+    use HasFactory;
+}
