acc1f1774ecdeead1be2d7985fa517fb38b509d8
EDU3RDOBR <eduardobfm5@gmail.com>
2024-04-29 14:25:55 -0300

    Update7.1

--- app/Http/Controllers/ImportControllerAPI.php
+++ app/Http/Controllers/ImportControllerAPI.php
@@ -13,7 +13,7 @@ class ImportControllerAPI extends Controller
     {
         // Faz uma requisição GET para a API com cabeçalhos de autorização e tipo de conteúdo
         $response = Http::withHeaders([
-            'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjU5Y2FiZDIyNDlmMWZkZWY0NDlkMDUyMzg3YzhmZmE2ODViYTNjZmM4NGQ3ODc0YmQwZjUyYjE0ZTk2ZDIxOTc5MzQ3ZWU2YjkyZDBjNmEzIn0.eyJhdWQiOiIxMTg5IiwianRpIjoiNTljYWJkMjI0OWYxZmRlZjQ0OWQwNTIzODdjOGZmYTY4NWJhM2NmYzg0ZDc4NzRiZDBmNTJiMTRlOTZkMjE5NzkzNDdlZTZiOTJkMGM2YTMiLCJpYXQiOjE3MTQzOTc4OTgsIm5iZiI6MTcxNDM5Nzg5OCwiZXhwIjoxNzE2OTg5ODk3LCJzdWIiOiI2NjQxIiwic2NvcGVzIjpbXX0.MeSHpJxQAAfB8EhqoNA7PxTOGlRtIjbKt7-_ayqQy9Zeep-xUvPIZUcKZHHMQ0SqOVHiVMWUTTWakIP5vPpHCOZhm-WAGkYXoBV-vG0njIKz4SCyczzuMpkMY8chyqO07uQctOmjDHOZEcbWfmPcT1Wey46uAQjKpD0TCrrfjsC7oi7jkNfjn6OGauKYwf2Kak5MnpL94Ask-T-cSYqp4pt92WFCO5vKnNzYOkPqiAvFouPrBjwVGoiy07KTasaHROGm0IrcW-HOaUyGdclZektg2ESO0lb0alx6qqw84qSXfFNg09WV4KJLx0Bx_qi87NwDbyXmiuQ2QQU0xLZy-uDX4-jbv00Ikx4vkbCeij1GUoSbCRsuEM4KyZloahot6Id6-iNCENcVsQcH-n9_NshivmDm09Fv1kcxaqi6o27oqyAC6YSPLinX_lz2DnabL0mouy31vYbgX1qxEIP0j5MDlGtLBOEumUsTvKISrMdE4pq534llc29w57deCwwG3zMwF6crlXez1UhOJTlREOk-xrCpEjWyBMJkKInTJnvz2TFALqXloXYAbgSagYbTRLrJXh1GvOJsWppL3clhhUYdjl8A0FTyuo4j9vHjvJLcUqtfjSfo6w5QcABKFxx0N8ePADCV2z7UrWtbwbS3xlnXhmGYKCpt_MC5qsCDCxM',
+            'Authorization' => 'Bearer ',
             'Content-Type' => 'application/json',
         ])->get('https://api.demo.hubsoft.com.br/api/v1/integracao/cliente', [
             'busca' => 'codigo_cliente',
